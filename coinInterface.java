c85676a115a808f87ccf20bc4caad61d57bfc929
GoldenlAkame <96786871+GoldenlAkame@users.noreply.github.com>
2021-12-28 21:01:45 +0000

    Update coinInterface.java

---
+++ coinInterface.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class coinInterface {
+    static Scanner ler = new Scanner(System.in);
+    public static void main(String[] args) {
+        double montanteTotal = 14.5, valorTotal = 0, troco = 0;
+        String resposta2 = "";
+        //receber valor para o montante total
+        System.out.println("Valor a pagar:" + montanteTotal);
+        valorTotal = introduzirMontante();
+        System.out.println("Deseja retirar o dinheiro? (Digite 'C' se desejar e qualquer outra para continuar para o pagamento)");
+        resposta2 = ler.nextLine();
+        if (resposta2.equalsIgnoreCase("C")){
+            valorTotal = 0;
+        } else {
+            if (montanteTotal > valorTotal) {
+                System.out.println("Valor insuficiente");
+                valorTotal = introduzirMontante();
+            } else {
+                troco = devolverTroco(montanteTotal, valorTotal);
+                System.out.println("Troco: " + troco);
+            }
+        }
+    }
+
+    public static double introduzirMontante() {
+        double valorRecebido = 0, valorTotal = 0;
+        String resposta = "";
+        do {
+            System.out.println("Introduza as moedas");
+            valorRecebido = lerMoeda();
+            ler.nextLine();
+            valorTotal = valorTotal + valorRecebido;
+            System.out.println("Valor total introduzido: " + valorTotal);
+            System.out.println("Deseja introduzir mais moedas? (sim ou nao)");
+            resposta = ler.nextLine();
+        } while (!resposta.equalsIgnoreCase("nao"));
+        return valorTotal;
+    }
+
+    public static double lerMoeda() {
+        double temp = 0;
+        int count = 0;
+        do {
+            if (count != 0){
+                System.out.println("Valor não é válido, introduza um valor válido");
+            }
+            temp = ler.nextDouble();
+            count++;
+        } while (temp != 0.01 && temp != 0.02 && temp != 0.05 && temp != 0.10 && temp != 0.2 && temp != 0.5 && temp != 1.0 && temp != 2.0);
+        return temp;
+    }
+
+    public static double devolverTroco(double montante, double valorIntroduzido) {
+        return calcularTroco(montante, valorIntroduzido);
+    }
+    
+    public static double calcularTroco(double montante, double valorIntroduzido) {
+        return valorIntroduzido - montante;
+    }
+}
