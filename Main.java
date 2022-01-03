348864e1dd8511e1fba4ddb2ec66e59e2aee22a7
drpacheco3 <tiagopacheco10@hotmail.com>
2022-01-03 18:23:25 +0000

    coinmaster e a parte dele no main

--- Main.java
+++ Main.java
@@ -1,19 +1,27 @@
 import java.util.concurrent.Semaphore;
 
-public class Main
+public class main
 {
     public static void main(String[] args) throws InterruptedException {
+
+        double montanteTotal = 14.5;
+        int users = -1,size=5;
+        Thread[] thscoinuser = new Thread[size];
+
+
         Semaphore semMT = new Semaphore( 0 );
         Buffer buffer = new Buffer();
         Thread t = new Thread( new Teclado( semMT, buffer ) );
-        coinMaster master= new coinMaster();
 
         t.start();
+        System.out.println("Escolha a opção na janela aberta");
 
         while ( true ) {
+
             semMT.acquire();
 
             String botao = buffer.getBotao();
+
 
             if ( botao.equals( "A" ) ) {
                 System.out.println( "Módulo Main: é para ABRIR a porta... a dar a ordem ao módulo Porta..." );
@@ -22,20 +30,30 @@ public class Main
                 System.out.println( "Módulo Main: é para FECHAR a porta... a dar a ordem ao módulo Porta..." );
             }
             else if ( botao.equals( "I" ) ) { // Iniciar Lavagem
-                System.out.println( "IIIIIIIIIIIIII" );
+                System.out.println( "I" );
             }
             else if ( botao.equals( "C" ) ) { // Camcelar Operacao
-                System.out.println( "CCCCCCCCCCCCCCC" );
+                System.out.println( "C" );
             }
             else if ( botao.equals( "E" ) ) { // pause the entire system
-                System.out.println( "EEEEEEEEEEEEEEEEEEEEEE" );
+                System.out.println( "E" );
             }
             else if ( botao.equals( "R" ) ) { //reset
-                System.out.println( "RRRRRRRRRRRRR" );
+                System.out.println( "R" );
             }
             else if ( botao.equals( "P" ) ) { //pay introduzir moedas
-                System.out.println( "INICIAR MODULO INTRODUZIR MOEDA..." );
-                coinMaster.introduzirMontante(master);
+                System.out.println( "P" );
+                users++;
+
+                if(users == thscoinuser.length){
+                    size *= 2;
+                    Thread[] arraycopy = new Thread[size];
+                    System.arraycopy(thscoinuser, 0, arraycopy, 0, size);
+                    thscoinuser = arraycopy;
+                }else {
+                    thscoinuser[users] = new Thread(new coinMaster(montanteTotal));
+                    thscoinuser[users].start();
+                }
             }
         }
 
--- coinMaster.java
+++ coinMaster.java
@@ -1,133 +1,76 @@
 import java.util.Scanner;
 
-public class coinMaster {
+public class coinMaster implements Runnable {
 
-    private static double CASH;
+    private static double Custo;
     double recieved;
     private byte handfull; //variavel para controlar se esta a reter dinheiro
 
-    public coinMaster(){
-        CASH = 0;
+    public coinMaster(double montanteTotal){
         recieved=0;
-        handfull=0;
+        Custo= montanteTotal;
     }
-
-
-
-    /*
-    public static void main(String[] args) {
-
-        double montanteTotal = 14.5, valorTotal = 0, troco = 0;
-        String resposta2 = "";
-        //receber valor para o montante total
-        System.out.println("Valor a pagar:" + montanteTotal);
-        valorTotal = introduzirMontante();
-        System.out.println("Deseja retirar o dinheiro? (Digite 'C' se desejar e qualquer outra para continuar para o pagamento)");
-        resposta2 = ler.nextLine();
-        if (resposta2.equalsIgnoreCase("C")){
-            valorTotal = 0;
-        } else {
-            if (montanteTotal > valorTotal) {
-                System.out.println("Valor insuficiente");
-                valorTotal = introduzirMontante();
-            } else {
-                troco = devolverTroco(montanteTotal, valorTotal);
-                System.out.println("Troco: " + troco);
-            }
-        }
-    }
-    */
 
     /**   DESCRICAO DE introduzirMontante
 
 
-    **/
+     **/
     public static void introduzirMontante(coinMaster user) { //provavelmente simplesmente inserir o valor logo de uma vez....
-       //Scanner ler = new Scanner(System.in);
-       double valorRecebido = 0;
-       String t= "";
-       /*do {
-           double temp;
-           System.out.println("Introduza as moedas");
-           temp = ler.nextDouble();
-           valorRecebido += temp;
-           System.out.println("Valor total introduzido: " + valorRecebido);
-           ler.reset();
-           System.out.println("Deseja introduzir mais moedas? (sim(y) ou nao(n))");
-           resposta = ler.nextLine();
-       } while (!resposta.equalsIgnoreCase("n")); */
 
+        double valorRecebido = 0;
+        String t= "";
 
-           valorRecebido = insertCoin();
-           t = ask();
-           if (t.equalsIgnoreCase("y"))
-           {
-               valorRecebido += insertCoin();
-           }
-           else
-           {
-            user.recieved = valorRecebido;
-            if(user.recieved!=0){user.handfull=1;}
-            else{ System.err.println(); }
-        }
-   }
+        valorRecebido = insertCoin();
 
- private static double insertCoin(){
-   Scanner ler = new Scanner(System.in);
-
-   double x;
-   System.out.println("Introduza as moedas");
-             x= ler.nextDouble();
-           return x;
-
-
- }
-
- private static String ask() {
-   Scanner ler = new Scanner(System.in);
-   String r = "";
-   System.out.println("Deseja introduzir mais moedas? (sim(y) ou nao(n))");
-             r = ler.nextLine();
-   if ( r.equalsIgnoreCase("y") || r.equalsIgnoreCase("n") ) {return r;}
-   else {ask(); return null;}
-}
-
-    /**   DESCRICAO DE devolverTroco
-
-
-    **/
-    public static void devolverTroco(double montante, double valorIntroduzido) {
-
+        user.recieved += valorRecebido;
+        if(user.recieved!=0){user.handfull=1;}
+        else{ System.err.println(); }
     }
 
 
+    private static double insertCoin(){
+        Scanner ler = new Scanner(System.in);
 
-    /**   DESCRICAO DE cancelOperation
-
-
-    **/
-    public static void cancelOperation(coinMaster user){
-      //codigo de cancelamento de operacoes
-
-
+        double x;
+        System.out.println("Introduza as moedas");
+        x= ler.nextDouble();
+        return x;
     }
 
-    /**   DESCRICAO DE lerMoeda
+
+    @Override
+    public void run() {
+        System.out.println( "INICIAR MODULO INTRODUZIR MOEDA..." );
+        Scanner ler = new Scanner(System.in);
+        double troco = 0;
+        String resposta2 = "";
 
 
-    **/
-    /*
-    public static double lerMoeda() {
-        double temp = 0;
-        int count = 0;
+        //receber valor para o montante total
+        System.out.println("Valor a pagar:" + Custo);
+        coinMaster.introduzirMontante(this);
+
         do {
-            if (count != 0){
-                System.out.println("Valor não é válido, introduza um valor válido");
+            System.out.println("Deseja retirar o dinheiro? (Digite 'C' se desejar e qualquer outra para continuar para o pagamento)");
+            resposta2 = ler.nextLine();
+            if (resposta2.equalsIgnoreCase("C")) {
+                this.recieved = 0;
+                System.out.println("Cancelado com sucesso");
+                break;
+            } else {
+                if (Custo > this.recieved) {
+                    System.out.println("Valor a pagar: " + Custo);
+                    System.out.println("Valor atual: " + this.recieved);
+                    System.out.println("Valor insuficiente");
+                    coinMaster.introduzirMontante(this);
+                } else {
+                    troco = this.recieved - Custo;
+                    System.out.println("Troco: " + troco);
+                    this.recieved = Custo;
+                    System.out.println("Espere a sua vez para iniciar a lavagem ");
+                }
             }
-            temp = ler.nextDouble();
-            count++;
-        } while (temp != 0.01 && temp != 0.02 && temp != 0.05 && temp != 0.10 && temp != 0.2 && temp != 0.5 && temp != 1.0 && temp != 2.0);
-        return temp;
+        }while (troco==0);
+        System.out.println("Escolha a opção na janela aberta");
     }
-    */
 }
